5478685aad73b73662a4b463b58243e77eff97a7
bielkoi <ktt.gabadrianputra1989@gmail.com>
2026-04-16 20:41:30 +0700

    Update biel-dkfj2ijiwsjdsa.js

--- js/biel-dkfj2ijiwsjdsa.js
+++ js/biel-dkfj2ijiwsjdsa.js
@@ -1,1 +1,44 @@
-
+const pageData = [
+  {
+    name: "KOITOTO_V1",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "profil",
+    url: "lp/formula1.html",
+  },
+  {
+    name: "Yu Gi OH",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/yugioh.html",
+  },
+  {
+    name: "Simple AMP",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/techno2.html",
+  },
+  {
+    name: "E-commerce Red Black",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/ecom.html",
+  },
+  {
+    name: "Gundam",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/gundam.html",
+  },
+  {
+    name: "Circle Logo",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/ecom2.html",
+  },
+];
